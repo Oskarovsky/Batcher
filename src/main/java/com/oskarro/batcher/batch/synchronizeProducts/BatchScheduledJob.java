57130ff08d900595e40756071cdd4fb36d20d474
Oskarovsky <oskar.slyk@gmail.com>
2021-07-24 15:59:49 +0200

    quartz trigger

---
+++ src/main/java/com/oskarro/batcher/batch/synchronizeProducts/BatchScheduledJob.java
@@ -0,0 +1,53 @@
+package com.oskarro.batcher.batch.synchronizeProducts;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BatchScheduledJob extends QuartzJobBean {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Qualifier("synchronizeProductsJob")
+    private final Job synchronizeProductsJob;
+
+    private final JobExplorer jobExplorer;
+    private final JobLauncher jobLauncher;
+
+
+    public BatchScheduledJob(@Qualifier("synchronizeProductsJob") Job synchronizeProductsJob,
+                             JobExplorer jobExplorer,
+                             JobLauncher jobLauncher) {
+        this.synchronizeProductsJob = synchronizeProductsJob;
+        this.jobExplorer = jobExplorer;
+        this.jobLauncher = jobLauncher;
+    }
+
+    /**
+     * executeInternal method will be called once each time the scheduled event fires
+     */
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        JobParameters jobParameters = new JobParametersBuilder(this.jobExplorer)
+                .getNextJobParameters(synchronizeProductsJob)
+                .toJobParameters();
+
+        try {
+            jobLauncher.run(synchronizeProductsJob, jobParameters);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/com/oskarro/batcher/batch/synchronizeProducts/QuartzConfiguration.java
@@ -0,0 +1,32 @@
+package com.oskarro.batcher.batch.synchronizeProducts;
+
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+
+@Configuration
+public class QuartzConfiguration {
+
+    @Bean
+    public JobDetail quartzJobDetail() {
+        return JobBuilder
+                .newJob(BatchScheduledJob.class)
+                .storeDurably()
+                .build();
+    }
+
+    @Bean
+    public Trigger jobTrigger() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder
+                .simpleSchedule()
+                .withIntervalInSeconds(5)
+                .withRepeatCount(4);
+
+        return TriggerBuilder.newTrigger()
+                .forJob(quartzJobDetail())
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+}
